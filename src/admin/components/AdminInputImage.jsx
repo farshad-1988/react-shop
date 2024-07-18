3951e34e1ef64e35bdb8758a564a23d99f6f7de8
farshad <far3farazdel@gmail.com>
2024-07-18 15:48:25 +0330

    recent commit

--- src/admin/components/AdminInputImage.jsx
+++ src/admin/components/AdminInputImage.jsx
@@ -1,67 +1,82 @@
-import { deleteObject, getDownloadURL, getStorage, ref, uploadBytesResumable } from "firebase/storage"
-import { useEffect } from "react"
-import { useCallback } from "react"
-import { useRef } from "react"
-import { useState } from "react"
-import { toast } from "react-toastify"
+import {
+  deleteObject,
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { useEffect } from "react";
+import { useCallback } from "react";
+import { useRef } from "react";
+import { useState } from "react";
+import { toast } from "react-toastify";
 
-
-const AdminInputImage = ({ items: { category, productId, dataUploaded }, getImagesUrl, getFromImages }) => {
-
-  const storage = getStorage()
-  const [formImages, setFormImages] = useState([])
-  const [imagesUrl, setImagesUrl] = useState([])
-  const [activeImage, setActiveImage] = useState(0)
-  const [progressBarContent, setProgressBarContent] = useState({})
-  const imageInputRef = useRef()
+const AdminInputImage = ({
+  items: { category, productId, dataUploaded },
+  getImagesUrl,
+  getFromImages,
+}) => {
+  const storage = getStorage();
+  const [formImages, setFormImages] = useState([]);
+  const [imagesUrl, setImagesUrl] = useState([]);
+  const [activeImage, setActiveImage] = useState(0);
+  const [progressBarContent, setProgressBarContent] = useState({});
+  const imageInputRef = useRef();
 
   const onMutate = async (e) => {
     if (e.target.files.length + formImages.length > 4) {
-      toast.error("maximum number of allowed image is four")
-      return
+      toast.error("maximum number of allowed image is four");
+      return;
     }
-    if (!e.target.files.length) return
+    if (!e.target.files.length) return;
 
     // if(e.target.files.length===4)return
     // categoryRef.current.disabled = true
     setFormImages((prev) => {
-      const repeated = prev.find((item) => (item.name === e.target.files[0].name))
-      if (repeated) return prev
+      const repeated = prev.find(
+        (item) => item.name === e.target.files[0].name
+      );
+      if (repeated) return prev;
       //  setImageBtnClass("btn btn-primary")
-      return [...prev, ...e.target.files]
-    })
+      return [...prev, ...e.target.files];
+    });
+  };
 
-  }
-
-  const storeImage = useCallback(() =>
-    async (image) => {
+  const storeImage = useCallback(
+    () => async (image) => {
       return new Promise((resolve, reject) => {
-        const fileName = `${image.name}`
-        const storageRef = ref(storage, `${category.toLocaleLowerCase().trim()}/${productId}/` + fileName)
+        const fileName = `${image.name}`;
+        const storageRef = ref(
+          storage,
+          `${category.toLocaleLowerCase().trim()}/${productId}/` + fileName
+        );
 
-        const uploadTask = uploadBytesResumable(storageRef, image)
+        const uploadTask = uploadBytesResumable(storageRef, image);
 
         uploadTask.on(
-          'state_changed',
+          "state_changed",
           (snapshot) => {
             const progress =
-              (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-            setProgressBarContent((prev) => ({ ...prev, [fileName]: progress }))
-
+              (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            setProgressBarContent((prev) => ({
+              ...prev,
+              [fileName]: progress,
+            }));
           },
           (error) => {
-            reject(error)
+            reject(error);
           },
           () => {
-            console.log("done")
+            console.log("done");
             getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-              resolve(downloadURL)
-            })
+              resolve(downloadURL);
+            });
           }
-        )
-      })
-    }, [category, productId, storage])
-
+        );
+      });
+    },
+    [category, productId, storage]
+  );
 
   const uploadImages = useCallback(async () => {
     return async () => {
@@ -69,91 +84,89 @@ const AdminInputImage = ({ items: { category, productId, dataUploaded }, getImag
 
       const imgUrls = await Promise.all(
         formImages.map((image) => {
-          if (progressBarContent[image.name] === 100) return
-          return storeImage(image)
+          if (progressBarContent[image.name] === 100) return;
+          return storeImage(image);
         })
       ).catch((error) => {
         // setLoading(false)
-        toast.error('Images not uploaded')
-        console.log(error)
-        return
-      })
+        toast.error("Images not uploaded");
+        console.log(error);
+        return;
+      });
       if (imgUrls) {
         imgUrls.map((url) => {
           if (url !== undefined) {
-            setImagesUrl((prev) => [...prev, url])
+            setImagesUrl((prev) => [...prev, url]);
           }
-        })
+        });
       }
-      getImagesUrl(imagesUrl, activeImage)
-    }
-
-  }, [activeImage, formImages, getImagesUrl, imagesUrl, progressBarContent, storeImage])
-
-
-
+      getImagesUrl(imagesUrl, activeImage);
+    };
+  }, [
+    activeImage,
+    formImages,
+    getImagesUrl,
+    imagesUrl,
+    progressBarContent,
+    storeImage,
+  ]);
 
   const removeAllImage = async () => {
-    if (!formImages.length) return
+    if (!formImages.length) return;
     if (window.confirm("confirm to remove all picture ?") === true) {
       const promisesRemovedItem = formImages.map(async (file) => {
-        const storageRef = ref(storage, `${category}/${productId}/` + file.name)
-        return await deleteObject(storageRef)
-      })
+        const storageRef = ref(
+          storage,
+          `${category}/${productId}/` + file.name
+        );
+        return await deleteObject(storageRef);
+      });
       try {
-        await Promise.all(promisesRemovedItem)
-        toast.success("successfully deleted")
-        setFormImages([])
-        setProgressBarContent({})
-        setImagesUrl([])
-        imageInputRef.current.value = null
+        await Promise.all(promisesRemovedItem);
+        toast.success("successfully deleted");
+        setFormImages([]);
+        setProgressBarContent({});
+        setImagesUrl([]);
+        imageInputRef.current.value = null;
       } catch (error) {
-        toast.error("there is a problem in removing pictures")
-        console.log(error)
+        toast.error("there is a problem in removing pictures");
+        console.log(error);
       }
     }
 
     //   categoryRef.current.disabled = false
-
-  }
+  };
 
   useEffect(() => {
-
-    getFromImages(formImages)
+    getFromImages(formImages);
     const uploadData = async () => {
-      await uploadImages()
-    }
-    uploadData()
-  }, [formImages, getFromImages, uploadImages])
-
+      await uploadImages();
+    };
+    uploadData();
+  }, [formImages, getFromImages, uploadImages]);
 
   useEffect(() => {
-
     if (dataUploaded) {
-      setFormImages([])
-      setProgressBarContent({})
-      setImagesUrl([])
+      setFormImages([]);
+      setProgressBarContent({});
+      setImagesUrl([]);
     }
-  }, [dataUploaded])
-
+  }, [dataUploaded]);
 
   useEffect(() => {
-    console.log("object")
+    console.log("object");
 
-    getImagesUrl(imagesUrl, activeImage)
-    if (!imagesUrl.length) imageInputRef.current.value = null
-  }, [activeImage, getImagesUrl, imagesUrl])
-
-
-
+    getImagesUrl(imagesUrl, activeImage);
+    if (!imagesUrl.length) imageInputRef.current.value = null;
+  }, [activeImage, getImagesUrl, imagesUrl]);
 
   const selectAciveImage = (e) => {
-    setActiveImage(e.target.value)
-  }
+    setActiveImage(e.target.value);
+  };
 
   function getPathStorageFromUrl(url) {
-
-    const baseUrl = "https://firebasestorage.googleapis.com/v0/b/shop2-8814c.appspot.com/o/";
+    const baseUrl =
+      "https://firebasestorage.googleapis.com/v0/b/shop2-8814c.appspot.com/o/";
 
     let imagePath = url.replace(baseUrl, "");
 
@@ -171,57 +184,102 @@ const AdminInputImage = ({ items: { category, productId, dataUploaded }, getImag
     // e.currentTarget.textContent = ""
     // e.currentTarget.className = "m-auto spinner-border"
     // e.currentTarget = <FontAwesomeIcon icon={faSpinner} />
-    const storageRef = ref(storage, `${category}/${productId}/` + image.name)
-    deleteObject(storageRef).then(() => {
-      toast.success(`${image.name} successfully deleted`)
-      const newFromImages = formImages.filter((file) => file.name !== image.name)
-      setFormImages(newFromImages)
-      const newImagesUrl = imagesUrl.filter((url) => getPathStorageFromUrl(url) !== `${category}/${productId}/` + image.name)
-      setImagesUrl(newImagesUrl)
-      setProgressBarContent((prev) => ({ ...prev, [image.name]: 0 }))
-      getImagesUrl(newImagesUrl, activeImage)
-      if (!newImagesUrl.length) imageInputRef.current.value = null
-    }).catch((error) => {
-      //double check
-      toast.error(error)
-    })
-  }
-
-
+    const storageRef = ref(storage, `${category}/${productId}/` + image.name);
+    deleteObject(storageRef)
+      .then(() => {
+        toast.success(`${image.name} successfully deleted`);
+        const newFromImages = formImages.filter(
+          (file) => file.name !== image.name
+        );
+        setFormImages(newFromImages);
+        const newImagesUrl = imagesUrl.filter(
+          (url) =>
+            getPathStorageFromUrl(url) !==
+            `${category}/${productId}/` + image.name
+        );
+        setImagesUrl(newImagesUrl);
+        setProgressBarContent((prev) => ({ ...prev, [image.name]: 0 }));
+        getImagesUrl(newImagesUrl, activeImage);
+        if (!newImagesUrl.length) imageInputRef.current.value = null;
+      })
+      .catch((error) => {
+        //double check
+        toast.error(error);
+      });
+  };
 
   return (
     <div className="d-flex flex-column">
       <input
         disabled={!category}
         ref={imageInputRef}
-        className='mb-3 mt-3 m-auto'
-        type='file'
-        id='images'
+        className="mb-3 mt-3 m-auto"
+        type="file"
+        id="images"
         onChange={onMutate}
         max="2"
-        accept='.jpg,.png,.jpeg'
+        accept=".jpg,.png,.jpeg"
         multiple
         required
       />
       {/* <button className={imageBtnClass} type='submit' ref={imageBtnRef}>upload images</button> */}
-      <div className='mt-2 d-flex'>
+      <div className="mt-2 d-flex">
         {formImages?.map((image, index) => {
-          return <div className='d-flex flex-column me-2 ' style={{ width: "100px" }} key={`adminImagesUpdated${index}`}>
-            {progressBarContent[image.name] === 100 && <span style={{ cursor: "pointer" }} className="m-auto badge text-danger " onClick={(e) => removeOneSelectedPhoto(e, image)}>X</span>}
-            <img src={URL.createObjectURL(image)} width={100} height={100} alt={image.name} />
-            <div className={`progress mt-2 ${!progressBarContent[image.name] ? "d-none" : "d-block"} }`}>
-              <div key={`progressBar${index}`} className={`progress-bar ${progressBarContent[image.name] === 100 && "bg-success"}`} style={{ width: `${progressBarContent[image.name]}%` }}>{Math.floor(progressBarContent[image.name])}%</div>
+          return (
+            <div
+              className="d-flex flex-column me-2 "
+              style={{ width: "100px" }}
+              key={`adminImagesUpdated${index}`}
+            >
+              {progressBarContent[image.name] === 100 && (
+                <span
+                  style={{ cursor: "pointer" }}
+                  className="m-auto badge text-danger "
+                  onClick={(e) => removeOneSelectedPhoto(e, image)}
+                >
+                  X
+                </span>
+              )}
+              <img
+                src={URL.createObjectURL(image)}
+                width={100}
+                height={100}
+                alt={image.name}
+              />
+              <div
+                className={`progress mt-2 ${!progressBarContent[image.name] ? "d-none" : "d-block"
+                  } }`}
+              >
+                <div
+                  key={`progressBar${index}`}
+                  className={`progress-bar ${progressBarContent[image.name] === 100 && "bg-success"
+                    }`}
+                  style={{ width: `${progressBarContent[image.name]}%` }}
+                >
+                  {Math.floor(progressBarContent[image.name])}%
+                </div>
+              </div>
+              <input
+                className="mt-1"
+                title="set as active image"
+                defaultChecked={index === 0}
+                type="radio"
+                name="selectPic"
+                value={index}
+                onChange={selectAciveImage}
+              />
             </div>
-            <input className="mt-1" title="set as active image" defaultChecked={index === 0} type="radio" name="selectPic" value={index} onChange={selectAciveImage} />
-          </div>
+          );
         })}
       </div>
-      {imagesUrl.length !== 0 && formImages.every((image) => progressBarContent[image.name] === 100) && <button className='btn btn-danger mt-4' onClick={removeAllImage}>
-        remove all images
-      </button>}
+      {imagesUrl.length !== 0 &&
+        formImages.every((image) => progressBarContent[image.name] === 100) && (
+          <button className="btn btn-danger mt-4" onClick={removeAllImage}>
+            remove all images
+          </button>
+        )}
     </div>
+  );
+};
 
-  )
-}
-
-export default AdminInputImage
+export default AdminInputImage;
