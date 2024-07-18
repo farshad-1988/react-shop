253491f08f032d641bac70d9419884267e166df9
farshad <far3farazdel@gmail.com>
2024-07-18 16:11:51 +0330

    some modification

--- src/admin/components/NavbarAdmin.jsx
+++ src/admin/components/NavbarAdmin.jsx
@@ -1,113 +1,155 @@
-import React from 'react'
-import homeLogo from '../../svg/homeLogo';
-import { Link, useNavigate } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMagnifyingGlass, faUserEdit } from '@fortawesome/free-solid-svg-icons';
-import SigningAndProfile from '../../layout/layoutComponents/SigningAndProfile';
-import { getCategoriesNameFromDB, searchFirestore } from '../../firebase.config';
-import { ShopContext } from '../../context/ShopContext';
-import { useContext } from 'react';
-import { UserContext } from '../../context/UserContext';
-import { useState } from 'react';
-import QueryResponsive from "../../utilities/QueryResponsive"
-
-
-
+import React from "react";
+import homeLogo from "../../svg/homeLogo";
+import { Link, useLocation, useNavigate, useParams } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faMagnifyingGlass,
+  faUserEdit,
+} from "@fortawesome/free-solid-svg-icons";
+import SigningAndProfile from "../../layout/layoutComponents/SigningAndProfile";
+import {
+  getCategoriesNameFromDB,
+  searchFirestore,
+} from "../../firebase.config";
+import { ShopContext } from "../../context/ShopContext";
+import { useContext } from "react";
+import { UserContext } from "../../context/UserContext";
+import { useState } from "react";
+import QueryResponsive from "../../utilities/QueryResponsive";
 
 const NavbarAdmin = () => {
+  const { category: selectedCategory } = useParams();
+  const { categoriesTitle } = useContext(ShopContext);
+  const location = useLocation();
+  const { currentUser } = useContext(UserContext);
+  const { lg, md } = QueryResponsive();
 
-    const { currentUser } = useContext(UserContext);
-    const { lg, md } = QueryResponsive()
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
-
-
-
-
-
-
-    const dayMessage = () => {
-        const hours = new Date().getHours()
-        let greet
-        if (hours < 12)
-            greet = "good morning";
-        else if (hours >= 12 && hours <= 17)
-            greet = "good afternoon";
-        else if (hours >= 17 && hours <= 24)
-            greet = "good evening"
-        return <div className='d-flex flex-column'><span>{greet}</span><span>{currentUser?.displayName || null}</span></div>
-    }
-
-
-    const { dispatch } = useContext(ShopContext);
-
-
-    const [searchText, setSearchText] = useState("");
-    const setInputValue = (e) => setSearchText(e.target.value);
-
-    const searchDb = async (e) => {
-        e.preventDefault();
-        dispatch({ type: "LOADING_PAGE_ON" });
-        const categories = await getCategoriesNameFromDB();
-        const allFoundedItems = await searchFirestore(categories.categories, searchText);
-        dispatch({ type: "SET_SEARCHED_ITEMS", payload: allFoundedItems });
-        navigate(`/admin/admineditproductbysearch/${searchText}`);
-        setSearchText("")
-    };
-
-
+  const dayMessage = () => {
+    const hours = new Date().getHours();
+    let greet;
+    if (hours < 12) greet = "good morning";
+    else if (hours >= 12 && hours <= 17) greet = "good afternoon";
+    else if (hours >= 17 && hours <= 24) greet = "good evening";
     return (
-        <>
-            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="d-flex flex-column">
+        <span>{greet}</span>
+        <span>{currentUser?.displayName || null}</span>
+      </div>
+    );
+  };
 
-                {lg && <Link to={"/"} className="align-self-start ms-3 d-flex align-items-center" >
-                    {homeLogo}
-                </Link>}
+  const { dispatch } = useContext(ShopContext);
 
+  const [searchText, setSearchText] = useState("");
+  const setInputValue = (e) => setSearchText(e.target.value);
 
-                <div className={`container-fluid d-flex h-auto ${md && "flex-column"}`}>
-                    {(currentUser && lg) && <div className="ms-5 text-capitalize lead ff-font-14" >{dayMessage()}</div>}
-                    <div className="m-auto col-12 col-sm-6 col-lg-4 align-self-center">
-                        <form className="input-group">
-                            <input
-                                onChange={setInputValue}
-                                type="text"
-                                className="form-control"
-                                placeholder="Search ..."
-                                value={searchText}
-                            />
-                            <button
-                                type="submit"
-                                onClick={(e) => searchDb(e)}
-                                className="input-group-text bg-success text-light"
-                            >
-                                <FontAwesomeIcon icon={faMagnifyingGlass} />
-                            </button>
-                            {/* <button type="button" className="input-group-text">
+  const searchDb = async (e) => {
+    e.preventDefault();
+    dispatch({ type: "LOADING_PAGE_ON" });
+    const categories = await getCategoriesNameFromDB();
+    const allFoundedItems = await searchFirestore(
+      categories.categories,
+      searchText
+    );
+    dispatch({ type: "SET_SEARCHED_ITEMS", payload: allFoundedItems });
+    navigate(`/admin/admineditproductbysearch/${searchText}`);
+    setSearchText("");
+  };
+
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        {lg && (
+          <Link
+            to={"/"}
+            className="align-self-start ms-3 d-flex align-items-center"
+          >
+            {homeLogo}
+          </Link>
+        )}
+
+        <div className={`container-fluid d-flex h-auto ${md && "flex-column"}`}>
+          {currentUser && lg && (
+            <div className="ms-5 text-capitalize lead ff-font-14">
+              {dayMessage()}
+            </div>
+          )}
+          <div className="m-auto col-12 col-sm-6 col-lg-4 align-self-center">
+            <form className="input-group">
+              <input
+                onChange={setInputValue}
+                type="text"
+                className="form-control"
+                placeholder="Search ..."
+                value={searchText}
+              />
+              <button
+                type="submit"
+                onClick={(e) => searchDb(e)}
+                className="input-group-text bg-success text-light"
+              >
+                <FontAwesomeIcon icon={faMagnifyingGlass} />
+              </button>
+              {/* <button type="button" className="input-group-text">
         <FontAwesomeIcon icon={faMicrophone} />
       </button> */}
-                        </form>
-                    </div>
-                    <div className={`d-flex justify-content-between  ${md && "w-100 mt-2"}`}>
+            </form>
+          </div>
+          <div
+            className={`d-flex justify-content-between  ${md && "w-100 mt-2"}`}
+          >
+            {md && <Link to={"/"}>{homeLogo}</Link>}
 
-                        {md && <Link to={"/"}>
-                            {homeLogo}
-                        </Link>}
+            <div className="d-flex align-items-center">
+              <Link
+                to={"/admin"}
+                className=" ms-3 d-flex align-items-center text-decoration-none"
+              >
+                <button className="d-flex btn btn-success me-4">
+                  <span className="me-2">Admin</span>
+                  <span>
+                    {
+                      <FontAwesomeIcon
+                        icon={faUserEdit}
+                        style={{ fontSize: "16px" }}
+                      />
+                    }
+                  </span>
+                </button>
+              </Link>
+              <SigningAndProfile />
+            </div>
+            {/* </div> */}
+          </div>
+        </div>
+      </nav>
+      <div className="d-flex justify-content-between">
+        <div>
+          {categoriesTitle.map((category, index) => (
+            <button
+              key={index}
+              onClick={() => navigate(`category/${category}`)}
+              className={`btn user-select-none text-capitalize ${
+                category === selectedCategory && "text-primary"
+              }`}
+            >
+              {category}
+            </button>
+          ))}
+        </div>
+        <button
+          onClick={() => navigate("/categories")}
+          className={`btn ${
+            location.pathname === "/categories" && "text-primary"
+          }`}
+        >
+          All Categories
+        </button>
+      </div>
+    </>
+  );
+};
 
-                        <div className="d-flex align-items-center">
-                             <Link to={"/admin"} className=" ms-3 d-flex align-items-center text-decoration-none">
-                                <button className='d-flex btn btn-success me-4'><span className='me-2'>Admin</span><span>{<FontAwesomeIcon icon={faUserEdit} style={{ fontSize: "16px" }} />}</span></button>
-                            </Link>
-                            <SigningAndProfile/>
-
-                        </div>
-                        {/* </div> */}
-                    </div>
-
-                </div>
-            </nav>
-        </>
-    )
-}
-
-export default NavbarAdmin
+export default NavbarAdmin;
--- src/components/EditUserData.jsx
+++ src/components/EditUserData.jsx
@@ -1,105 +1,183 @@
-import { useForm } from "react-hook-form"
-import * as yup from "yup"
-import {yupResolver} from '@hookform/resolvers/yup'
-import { editDocumentUser} from "../firebase.config";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { editDocumentUser } from "../firebase.config";
 import { useNavigate, useParams } from "react-router-dom";
 import { useContext } from "react";
 import { UserContext } from "../context/UserContext";
 
+const EditUserData = () => {
+  const navigate = useNavigate();
+  const { userId } = useParams();
+  const { currentUser, userDoc } = useContext(UserContext);
 
+  const { name, lastName, phoneNumber, address } = { ...userDoc };
 
-const EditUserData = ()=>{
-    const navigate = useNavigate()
-    const {userId} = useParams()
-    const {currentUser , userDoc} = useContext(UserContext)
+  const userSchema = yup.object().shape({
+    name: yup.string().min(3).required("please enter a valid name"),
+    lastName: yup.string().min(3).required("please enter a valid family"),
+    // email: yup.string().email("you must enter valid email address").required("enter email address"),
+    phoneNumber: yup.number().min(10).required("enter phone number"),
+    address: yup.object({
+      address1: yup.string().max(30).required("please enter your address"),
+      address2: yup.string().max(30),
+      city: yup.string().max(20).required("please enter your city"),
+      district: yup
+        .string()
+        .max(20)
+        .required("please enter your district or state"),
+      zipCode: yup.string().max(10).required("please enter your zip code"),
+      country: yup.string().max(20).required("please enter your country"),
+    }),
+  });
 
-    const {name , lastName  , phoneNumber , address}={...userDoc}
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(userSchema),
+    defaultValues: { name, lastName, phoneNumber, address },
+  });
 
-    const userSchema = yup.object().shape({
-        name: yup.string().min(3).required("please enter a valid name"),
-        lastName: yup.string().min(3).required("please enter a valid family"),
-        // email: yup.string().email("you must enter valid email address").required("enter email address"),
-        phoneNumber: yup.number().min(10).required("enter phone number"),
-        address: yup.object({
-            address1 : yup.string().max(30).required("please enter your address"),
-            address2 : yup.string().max(30),
-            city : yup.string().max(20).required("please enter your city"),
-            district : yup.string().max(20).required("please enter your district or state"),
-            zipCode : yup.string().max(10).required("please enter your zip code"),
-            country : yup.string().max(20).required("please enter your country") 
-        })
-    })
-    
-    const {register , handleSubmit , formState:{errors}} = useForm({ resolver:yupResolver(userSchema),
-        "defaultValues":{name , lastName  , phoneNumber , address}
-    })
+  const submitFormToDB = async ({ name, lastName, address, phoneNumber }) => {
+    if (!currentUser) return;
+    await editDocumentUser(
+      currentUser.uid,
+      name,
+      lastName,
+      address,
+      phoneNumber
+    );
+    navigate(`/profile/${userId}`);
+  };
 
-    const submitFormToDB = async ({name , lastName,address , phoneNumber})=>{
-        if(!currentUser) return
-        await editDocumentUser(currentUser.uid  ,name , lastName ,address, phoneNumber)
-        navigate(`/profile/${userId}`)
-    }
-
-    return (
-        <form className="container " onSubmit={handleSubmit(submitFormToDB)}>
-            <div className="mt-2">
-                <span>first name:</span>
-                <input className="form-control" type="text" placeholder="please enter your firstname..." {...register("name")} />
-                {errors.name?.message && <p className="text-danger">{errors.name.message}</p>}
-            </div>
-            <div className="mt-2">
-                <span>last name:</span>
-                <input className="form-control" type="text" placeholder="please enter your lastname..." {...register("lastName")}/>
-                {errors.lastName?.message && <p className="text-danger">{errors.lastName.message}</p>}
-            </div>
-            {/* <div className="mt-2">
+  return (
+    <form className="container " onSubmit={handleSubmit(submitFormToDB)}>
+      <div className="mt-2">
+        <span>first name:</span>
+        <input
+          className="form-control"
+          type="text"
+          placeholder="please enter your firstname..."
+          {...register("name")}
+        />
+        {errors.name?.message && (
+          <p className="text-danger">{errors.name.message}</p>
+        )}
+      </div>
+      <div className="mt-2">
+        <span>last name:</span>
+        <input
+          className="form-control"
+          type="text"
+          placeholder="please enter your lastname..."
+          {...register("lastName")}
+        />
+        {errors.lastName?.message && (
+          <p className="text-danger">{errors.lastName.message}</p>
+        )}
+      </div>
+      {/* <div className="mt-2">
                 <span>email address:</span>
                 <input className="form-control" type="text" placeholder="please enter your email..." {...register("email")}/>
                 {errors.email?.message && <p className="text-danger">{errors.email.message}</p>}
             </div> */}
-            <div className="mt-2">
-                <span>phone number</span>
-                <input className="form-control" type="number" placeholder="please enter your phone number..." {...register("phoneNumber")}/>
-                {errors.phoneNumber?.message && <p className="text-danger">{errors.phoneNumber.message}</p>}
-            </div>
+      <div className="mt-2">
+        <span>phone number</span>
+        <input
+          className="form-control"
+          type="number"
+          placeholder="please enter your phone number..."
+          {...register("phoneNumber")}
+        />
+        {errors.phoneNumber?.message && (
+          <p className="text-danger">{errors.phoneNumber.message}</p>
+        )}
+      </div>
 
-            
-            <div className="mt-2">
-                <span>address1:</span>
-                <input className="form-control" type="text" placeholder="please enter your postal address..." {...register("address.address1")}/>
-                {errors.address?.address1?.message && <p className="text-danger">{errors.address?.address1.message}</p>}
-            </div>
-            <div className="mt-2">
-                <span>address2:</span>
-                <input className="form-control" type="text" placeholder="please enter your postal address..." {...register("address.address2")}/>
-                {errors.address?.address2?.message && <p className="text-danger">{errors.address?.address2.message}</p>}
-            </div>
-            <div className="mt-2">
-                <span>city:</span>
-                <input className="form-control" type="text" placeholder="please enter your postal address..." {...register("address.city")}/>
-                {errors.address?.city?.message && <p className="text-danger">{errors.address?.city.message}</p>}
-            </div>
-            <div className="mt-2">
-                <span>district:</span>
-                <input className="form-control" type="text" placeholder="please enter your postal address..." {...register("address.district")}/>
-                {errors.address?.district?.message && <p className="text-danger">{errors.address?.district.message}</p>}
-            </div>
-            <div className="mt-2">
-                <span>zipCode:</span>
-                <input className="form-control" type="text" placeholder="please enter your postal address..." {...register("address.zipCode")}/>
-                {errors.address?.zipCode?.message && <p className="text-danger">{errors.address?.zipCode.message}</p>}
-            </div>
-            <div className="mt-2">
-                <span>country:</span>
-                <input className="form-control" type="text" placeholder="please enter your postal address..." {...register("address.country")}/>
-                {errors.address?.country?.message && <p className="text-danger">{errors.address?.country.message}</p>}
-            </div>
-            <div className="text-center">
-                <button className="mt-3 btn btn-primary w-50" type="submit">submit info</button>
-                <button type="button" className="btn">cancle</button>
-            </div>
-        </form>
-    )
-}
+      <div className="mt-2">
+        <span>address1:</span>
+        <input
+          className="form-control"
+          type="text"
+          placeholder="please enter your postal address..."
+          {...register("address.address1")}
+        />
+        {errors.address?.address1?.message && (
+          <p className="text-danger">{errors.address?.address1.message}</p>
+        )}
+      </div>
+      <div className="mt-2">
+        <span>address2:</span>
+        <input
+          className="form-control"
+          type="text"
+          placeholder="please enter your postal address..."
+          {...register("address.address2")}
+        />
+        {errors.address?.address2?.message && (
+          <p className="text-danger">{errors.address?.address2.message}</p>
+        )}
+      </div>
+      <div className="mt-2">
+        <span>city:</span>
+        <input
+          className="form-control"
+          type="text"
+          placeholder="please enter your postal address..."
+          {...register("address.city")}
+        />
+        {errors.address?.city?.message && (
+          <p className="text-danger">{errors.address?.city.message}</p>
+        )}
+      </div>
+      <div className="mt-2">
+        <span>district:</span>
+        <input
+          className="form-control"
+          type="text"
+          placeholder="please enter your postal address..."
+          {...register("address.district")}
+        />
+        {errors.address?.district?.message && (
+          <p className="text-danger">{errors.address?.district.message}</p>
+        )}
+      </div>
+      <div className="mt-2">
+        <span>zipCode:</span>
+        <input
+          className="form-control"
+          type="text"
+          placeholder="please enter your postal address..."
+          {...register("address.zipCode")}
+        />
+        {errors.address?.zipCode?.message && (
+          <p className="text-danger">{errors.address?.zipCode.message}</p>
+        )}
+      </div>
+      <div className="mt-2">
+        <span>country:</span>
+        <input
+          className="form-control"
+          type="text"
+          placeholder="please enter your postal address..."
+          {...register("address.country")}
+        />
+        {errors.address?.country?.message && (
+          <p className="text-danger">{errors.address?.country.message}</p>
+        )}
+      </div>
+      <div className="text-center d-flex align-items-center justify-content-center mt-4 mb-5">
+        <button className="btn btn-primary" type="submit">
+          Submit Information
+        </button>
+        <button type="button" className="btn">
+          Cancle
+        </button>
+      </div>
+    </form>
+  );
+};
 
-export default EditUserData
+export default EditUserData;
